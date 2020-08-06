06c6ab22e7f2d5b06bca3f89fb951a8314cdbae9
PersiLiao <xiangchu.liao@gmail.com>
2020-08-07 00:31:25 +0800

    update

---
+++ src/DotEnv.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Persi.Liao
+ * @email xiangchu.liao@gmail.com
+ * @link https://www.github.com/persiliao
+ */
+
+declare(strict_types=1);
+
+namespace PersiLiao\Utils;
+
+use Dotenv\Repository\Adapter\EnvConstAdapter;
+use Dotenv\Repository\Adapter\PutenvAdapter;
+use Dotenv\Repository\RepositoryBuilder;
+
+class DotEnv
+{
+    public static function load(string $paths): void
+    {
+        $repository = RepositoryBuilder::createWithNoAdapters()
+            ->addAdapter(EnvConstAdapter::class)
+            ->addWriter(PutenvAdapter::class)
+            ->immutable()
+            ->make();
+        \Dotenv\Dotenv::create($repository, $paths)->load();
+    }
+}
