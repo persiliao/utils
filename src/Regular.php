3718011f7744e26ffb15a94f161ea31fb9a384a7
PersiLiao <xiangchu.liao@gmail.com>
2021-06-23 20:38:58 +0800

    Add regular

---
+++ src/Regular.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PersiLiao\Utils;
+
+class Regular{
+    
+    const HTML_IMAGE = '/<img.*?src=[\'|\"](.*?(?:[\.gif|\.png|\.jpg|\.bmp|\.jpeg|\.webp]))[\'|\"].*?[\/]?>/i';
+}
